92db0d29b55c8e01db7351c37310531c26258294
Shofiq Saif <sislam161079@gmail.com>
2023-06-21 11:36:15 +0600

    Added player input sequence checker.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var gamePattern = [];
 var isRunning = false;
 var colors = ["green", "red", "yellow", "blue"];
+var i = 0;
 
 function nextSequence() {
   // Return a number between 0-3
@@ -12,37 +13,47 @@ function resetGame() {
   var newSequence = nextSequence();
 }
 
-function addNextSequence() {
-  var next = nextSequence();
-  gamePattern.push(colors[next]);
-
-  $("." + colors[next]).animate({
+function animateAndSound(color) {
+  $("." + color).animate({
     opacity: 0.2,
   });
-  $("." + colors[next]).animate({
+  $("." + color).animate({
     opacity: 1,
   });
 
-  var sound = new Audio("/sounds/" + colors[next] + ".mp3");
+  var sound = new Audio("/sounds/" + color + ".mp3");
   sound.play();
+}
 
+function addNextSequence() {
+  var next = nextSequence();
+  gamePattern.push(colors[next]);
+  animateAndSound(colors[next]);
   $("h1").text("Level " + gamePattern.length);
 }
 
-$(".btn").on('click',function () {
-        var colorClicked = (this.getAttribute('class').slice(4,100));
-        
-        if(colorClicked == gamePattern[gamePattern.length-1]){
-                addNextSequence();
-                $("h1").text("Level " + gamePattern.length);
-        }
-        else{
-                $("h1").text("Game Over, Press Any Key to Restart");
-                isRunning = false;
-                var audio = new Audio("/sounds/wrong.mp3");
-                audio.play();
-        }
-      });
+$(".btn").on("click", function () {
+  var colorClicked = this.getAttribute("class").slice(4, 100);
+
+  if (colorClicked == gamePattern[i]) {
+    animateAndSound(colorClicked);
+    i++;
+
+    if (i == gamePattern.length) {
+      $("h1").text("Level " + gamePattern.length);
+      i = 0;
+      setTimeout(function (){
+        addNextSequence();
+      },500);
+      
+    }
+  } else {
+    $("h1").text("Game Over, Press Any Key to Restart");
+    isRunning = false;
+    var audio = new Audio("/sounds/wrong.mp3");
+    audio.play();
+  }
+});
 
 $(document).keypress(function () {
   if (!isRunning) {
